0ec18418fba85fc59f50e47c93df538e07bf28a6
sarahmurphy86 <sarah.graham86@gmail.com>
2018-06-01 14:53:14 +0100

    dinosaur abstract class created

--- src/main/java/Dinosaurs/Dinosaur.java
+++ src/main/java/Dinosaurs/Dinosaur.java
@@ -1,4 +1,16 @@
 package Dinosaurs;
 
 public abstract class Dinosaur {
+
+    private String name;
+    private DietType dietType;
+    private int hungerLevel;
+    private double weight;
+
+    public Dinosaur(String name, DietType dietType, int hungerLevel, double weight){
+        this.name = name;
+        this.dietType = dietType;
+        this.weight = weight;
+    }
+
 }
